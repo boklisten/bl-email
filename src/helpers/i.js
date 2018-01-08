bacb67f4f73dc5aa54d41dc66e80f8d145a13983
holskil <aholskil@gmail.com>
2018-01-08 14:14:38 +0100

    feat: i now inserts handlebars based on prod or test

--- src/helpers/i.js
+++ src/helpers/i.js
@@ -1,9 +1,43 @@
 /**
  * @example
- * {{i username}}
+ * {{i 'emailTemplateConfig.name'}}
  * @outputs
- * {{username}}
+ * {{email.TemplateConfig.name}} in prod
+ * and "aName" in test
  */
 module.exports = function (rawHandlebar) {
-   return "{{" + rawHandlebar + "}}";
+
+
+    const blEmailConfig = require('../../bl-email.config.json');
+
+    if (!blEmailConfig.prod) {
+        try {
+
+            const emailTemplateConfig = require('../data/emailTemplateConfig.json');
+            const emailTemplateInput = require('../data/emailTemplateInput.json');
+            let splitVarPath = rawHandlebar.split('.');
+            let varType = splitVarPath[0];
+            let value = '';
+
+            if (varType === 'emailTemplateConfig') {
+                value = emailTemplateConfig;
+                for (let i = 1; i < splitVarPath.length; i++) {
+                    value = value[splitVarPath[i]];
+                }
+            } else if (varType === 'emailTemplateInput') {
+                value = emailTemplateInput;
+                for (let i = 1; i < splitVarPath.length; i++) {
+                    value = value[splitVarPath[i]];
+                }
+            }
+
+            if (!value) throw new Error('value not found');
+
+            return value;
+        } catch (e) {
+            return "";
+        }
+    }
+
+    return "{{" + rawHandlebar + "}}";
 };
