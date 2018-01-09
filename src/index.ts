eb601ae710ad4bd88fe1d5d16a1a765680b0e76b
holskil <aholskil@gmail.com>
2018-01-09 13:09:32 +0100

    fix: added emailTemplateInput and emailTemplateConfig to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export {EmailHandler} from './ts/email.handler';
 export {EmailLog} from './ts/email-log';
 export {EmailType} from './ts/template/email-type';
+export {EmailTemplateConfig} from './ts/template/email-template-config';
+export {EmailTemplateInput} from './ts/template/email-template-input';
